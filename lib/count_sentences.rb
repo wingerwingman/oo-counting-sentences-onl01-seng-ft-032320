50f7de8261ecd860f3e65c286d355ad3a5aa0184
Justin Winger <justinwinger4@gmail.com>
2020-03-30 19:18:43 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true 
+    else 
+      false 
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true 
+    else 
+      false 
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true 
+    else 
+      false 
+    end
   end
 
   def count_sentences
+    arr = self.split(/[?.!] /)
+    arr.length
+    # binding.pry
 
   end
 end
